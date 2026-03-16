406fd393e39e8dbb6e4690fe6223b4a9502e4791
jordan p <jordanp5794@gmail.comgit config --global user.name jordan>
2026-03-16 08:39:43 -0500

    Completed character checker program

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
-const input = require('readline-sync');
-let name = input.question("What is your name? ");
+const readlineSync = require('readline-sync');
 
-console.log("Hello " + name + "! Lets get to know you.");
+let userInput = readlineSync.question("Enter a word or phrase: ");
+let indexNumber = readlineSync.questionInt("Enter an index number: ");
 
-let q1 = input.question("1. What is your favorite food? ");
-let q2 = input.question("2. What is your favorite color? ");
-let q3 = Number(input.question("3. What is 5 + 7? "));
-let q4 = input.question("4. What is your job title'? ");
-let q5 = input.question("5. What is your favorite movie? ");
+let character = userInput[indexNumber];
 
-console.log("\nThanks for answering:");
-console.log("1:", q1);
-console.log("2:", q2);
-console.log("3:", q3);
-console.log("4:", q4);
-console.log("5:", q5);
+console.log("The character at index " + indexNumber + " is: " + character);
